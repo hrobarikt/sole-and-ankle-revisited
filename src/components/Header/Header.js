136e48ee1c7c4ec35016c1b47bd33f8c6d2f1bbe
hrobarikt <tomas.hrobarik@gmail.com>
2022-04-21 01:00:09 +0200

    exercise 5

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -61,8 +61,8 @@ const MainHeader = styled.div`
   display: flex;
   align-items: baseline;
   padding: 18px 32px;
-  height: 72px;
   border-bottom: 1px solid ${COLORS.gray[300]};
+  overflow: auto;
 
   @media ${QUERIES.tabletAndSmaller} {
     border-top: 4px solid ${COLORS.gray[900]};
@@ -78,7 +78,7 @@ const MainHeader = styled.div`
 
 const Nav = styled.nav`
   display: flex;
-  gap: 48px;
+  gap: clamp(1rem, 8vw - 4rem, 3rem);
   margin: 0px 48px;
 
   @media ${QUERIES.tabletAndSmaller} {
